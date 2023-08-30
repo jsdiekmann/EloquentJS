2b3523fa0cf4cfbf3a66aa29fd943044439be594
jsdiekmann <97804946+jsdiekmann@users.noreply.github.com>
2023-08-29 20:33:57 -0400

    Handling negative numbers in recursion problem

--- Chapter 3/recursion.js
+++ Chapter 3/recursion.js
@@ -1,4 +1,11 @@
 const isEven = num => {
+    
+    //Handles negative numbers (makes them positive)
+    if(num<0) {
+        num = num * (-1);
+    }
+    
+    //Recursive function to determine if a positive integer is odd or even
     if(num === 0) {
         return 'even';
     } else if(num === 1) {
